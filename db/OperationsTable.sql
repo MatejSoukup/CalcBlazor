1b788982e3f7e76fcdd5bb652dc2d6a46a800bd3
MatejSoukup <4mates.sou@gmail.com>
2023-11-16 21:37:23 +0100

    testing and sql table

---
+++ db/OperationsTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Operations] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [leftNumber]   VARCHAR (255)  NULL,
+    [rightNumber]  VARCHAR (255)  NULL,
+    [mathOperator] VARCHAR (255)  NULL,
+    [result]       NVARCHAR (255) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
